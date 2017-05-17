f2d2d1920b7245365cb6d6eb49a54bdf46479bbf
Your Name <you@example.com>
2017-05-17 14:25:52 -0600

    angular project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,16 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+  angular.module('chatroom').controller('mainCtrl', function($scope, messageService) {
+  $scope.getMessages = function() {
+
+  	messageService.getMessages().then(function(response) {
+      $scope.messages = response.data.reverse();
+      console.log($scope.messages)
+  });
+  }
+
+  $scope.getMessages();
+
+  
+ 
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
@@ -10,13 +22,18 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
-
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+$scope.postMessage = function(message){
+  console.log(message)
+messageService.postMessage(message).then(function(response){
 
 })
+}
+  // uncomment this code when your getMessages function is finished
+  // This goes and gets new data every second, which mimicking a chat room experience.
+
+   setInterval(function(){
+    $scope.getMessages();
+
+  }, 1500)
+});
+
